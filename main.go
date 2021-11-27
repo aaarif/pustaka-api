3b5bb841aa458d81d8e353605cc88445b4fbbb82
Arif <arif680@gmail.com>
2021-11-28 00:13:46 +0700

    MySQL DB Connection

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"pustaka-api/handler"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
+
+	dsn := "root:root@tcp(127.0.0.1:3306)/pustaka-api?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("DB Connection Error")
+	}
+	fmt.Println("Database connected.")
 	router := gin.Default()
 	v1 := router.Group("/v1")
 	v1.GET("/", handler.RootHandler)
